83423dee37e0cd28e6a9b3d9a24e42fb4dd2a27f
Luz Zabaleta <zluzrq@gmail.com>
2024-08-16 12:57:04 -0400

    actualizacion

--- app/src/main/java/com/example/class02/registrarProducto.kt
+++ app/src/main/java/com/example/class02/registrarProducto.kt
@@ -1,36 +1,20 @@
 package com.example.class02
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.content.Intent
-class Login : AppCompatActivity() {
-    lateinit var btnIngresar: Button
+
+class registrarProducto : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_registrar_producto)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        cargarR()
-        estadoBoton()
-    }
-
-    fun cargarR(){
-        btnIngresar = findViewById(R.id.btnLogin)
-    }
-
-    fun estadoBoton(){
-        btnIngresar.setOnClickListener{
-            val i = Intent(this, Home::class.java)
-            startActivity(i)
         }
     }
 }
