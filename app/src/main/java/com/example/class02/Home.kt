0db386d9b4994d2856cc9d5ee3f71bac667b9aa7
Luz Zabaleta <zluzrq@gmail.com>
2024-08-21 13:01:31 -0400

    Actualizado

--- app/src/main/java/com/example/class02/Home.kt
+++ app/src/main/java/com/example/class02/Home.kt
@@ -1,12 +1,17 @@
 package com.example.class02
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import androidx.cardview.widget.CardView
 
 class Home : AppCompatActivity() {
+    lateinit var btnIvaPro: CardView
+    lateinit var btnProduct: CardView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,25 @@ class Home : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        cargarR()
+
+        estadoBoton()
     }
+
+    fun cargarR(){
+        btnIvaPro = findViewById(R.id.btnCalcularIvaProduct)
+        btnProduct = findViewById(R.id.btnProductos)
+    }
+
+    fun estadoBoton(){
+        btnIvaPro.setOnClickListener{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
+        btnProduct.setOnClickListener{
+            val i = Intent(this, registrarProducto::class.java)
+            startActivity(i)
+        }
+    }
+
 }
